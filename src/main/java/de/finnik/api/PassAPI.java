0f5aad8a53999600835685801c69b977657eecde
finnik <de.finnik@gmx.net>
2020-04-09 10:57:37 +0200

    Added javadoc to PassAPI.java

--- src/main/java/de/finnik/api/PassAPI.java
+++ src/main/java/de/finnik/api/PassAPI.java
@@ -12,8 +12,20 @@ import java.util.stream.*;
 
 import static de.finnik.gui.Var.*;
 
+/**
+ * Allows you to kind of manage your passwords out of the command line.
+ * If you append arguments to the execution this constructor will be fired.
+ *
+ * @see PassAPI#PassAPI(String[])
+ */
 public class PassAPI {
 
+    /**
+     * Takes the arguments that are given to the main application
+     *
+     * @param args The arguments
+     * @see PassVault#main(String[])
+     */
     public PassAPI(String[] args) {
         try {
             Method method = PassAPI.class.getDeclaredMethod(args[0], String[].class);
@@ -30,6 +42,12 @@ public class PassAPI {
         }
     }
 
+    /**
+     * Allows you to add a password to the passwords list just from arguments.
+     * Expects a json object as input with keys pass, site, user, other and matching values.
+     *
+     * @param args The input json object
+     */
     @Command(description = "Add a password to the passwords list")
     private static void add(String[] args) {
         Password password = null;
@@ -54,6 +72,12 @@ public class PassAPI {
         }).setVisible(true);
     }
 
+    /**
+     * Allows you to get all passwords matching to a keyword.
+     * Expects a keyword as input and optionally -p as argument to enable pretty printing for the output json objects.
+     *
+     * @param args The key word and if necessary -p option
+     */
     @Command(description = "Search a password matching with a given keyword")
     private static void get(String[] args) {
         if (args[0].length() < 3) {
@@ -75,17 +99,34 @@ public class PassAPI {
         }, String.format(LANG.getProperty("api.get.warning"), args[0])).setVisible(true);
     }
 
+    /**
+     * Allows you to get information about which version of PassVault is installed.
+     * Expects no arguments
+     *
+     * @param args Empty
+     */
     @Command(description = "Get information about the installed version of PassVault")
     private static void version(String[] args) {
         System.out.printf("%s %s by %s", APP_INFO.getProperty("app.name"), APP_INFO.getProperty("app.version"), APP_INFO.getProperty("app.author"));
         LOG.info("User got information about the installed version of PassVault!");
     }
 
+    /**
+     * Will warn the user that the operation wasn't successful and stop the program via {@link System#exit(int)}
+     *
+     * @param code    The error code
+     * @param message The error message to display
+     */
     private static void error(int code, String message) {
         System.out.printf("!%d %s", code, message);
-        System.exit(0);
+        System.exit(1);
     }
 
+    /**
+     * Prints help for a given command
+     *
+     * @param command The command to get more information from
+     */
     private void printHelp(String command) {
         InputStream inputStream = PassVault.class.getResourceAsStream("/api_help/" + command + ".help");
         if (inputStream == null)
@@ -96,6 +137,12 @@ public class PassAPI {
                 .forEach(description -> System.out.printf(new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining(System.lineSeparator())), description));
     }
 
+    /**
+     * Prints all valid commands and what they can be used for.
+     * Is executed when the user enters an invalid command.
+     *
+     * @param command The invalid command entered by the user
+     */
     private void printCommands(String command) {
         System.out.printf("%s is not a valid command! You should probably use one of the following commands:\n\n", command);
         Arrays.stream(PassAPI.class.getDeclaredMethods())
@@ -105,6 +152,9 @@ public class PassAPI {
         System.out.println("\t<command> help\n\t\t\tGet help for the command <command>\n");
     }
 
+    /**
+     * Marks all valid command functions and informs the user about their functionality
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
     private @interface Command {
